46c8de53f5d79d3ee6b438b297636ebf919f347e
amirreza <taghaddosiamirreza@gmail.com>
2023-10-09 19:55:54 +0330

    view for <News By Category API>: return all news that have relation with this category(by giving ID or title of category)

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import News
+from .serializers import NewsByCategorySerializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
-# Create your views here.
+class NewsByCategoryView(generics.ListAPIView):
+    serializer_class = NewsByCategorySerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('category_id'):
+            category_id = self.kwargs['category_id']
+            queryset = News.objects.filter(category__id=category_id)
+
+            if not queryset.exists():
+                raise Http404()
+            
+        elif self.kwargs.get('category_title'):
+            category_title = self.kwargs['category_title']
+            queryset = News.objects.filter(category__title=category_title)
+
+            if not queryset.exists():
+                raise Http404() 
+            
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
+    
