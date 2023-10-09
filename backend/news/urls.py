f9f56cc0904342d25c3ab3fb431b3a28d2b6f9d0
amirreza <taghaddosiamirreza@gmail.com>
2023-10-09 20:09:11 +0330

    url for <News By Category API>: giving ID or title of category

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import NewsByCategoryView 
 
 urlpatterns = [
-    path('category/news/<int:category_id>/', NewsByCategoryView.as_view(), name='newsByCategory_list'),
+    path('category/news/<int:category_id>/', NewsByCategoryView.as_view(), name='newsByCategoryID_list'),
+    path('category/news/<str:category_title>/', NewsByCategoryView.as_view(), name='newsByCategoryTitle_list'),
 ]
-
-
