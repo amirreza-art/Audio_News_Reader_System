d8310523db853e988ef9497a94a3de2ae41c940c
amirreza <taghaddosiamirreza@gmail.com>
2023-10-10 13:51:56 +0330

    url for <News Detail API>: give guid

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NewsByCategoryView 
+from .views import NewsByCategoryView, NewsDetailView
 
 urlpatterns = [
     path('category/news/<int:category_id>/', NewsByCategoryView.as_view(), name='newsByCategoryID_list'),
     path('category/news/<str:category_title>/', NewsByCategoryView.as_view(), name='newsByCategoryTitle_list'),
+    path('the-news/<int:guid>/', NewsDetailView.as_view(), name='NewsByGuid_detail'),
 ]
