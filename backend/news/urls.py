a158d962edaae1a1e4e2898b1cafb8098ddc49cf
amirreza <taghaddosiamirreza@gmail.com>
2023-10-10 23:32:51 +0330

    url for <Category Lisy API>

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewsByCategoryView, NewsDetailView
+from .views import NewsByCategoryView, NewsDetailView, CategoryListView
 
 urlpatterns = [
     path('category/news/<int:category_id>/', NewsByCategoryView.as_view(), name='newsByCategoryID_list'),
     path('category/news/<str:category_title>/', NewsByCategoryView.as_view(), name='newsByCategoryTitle_list'),
     path('the-news/<int:guid>/', NewsDetailView.as_view(), name='NewsByGuid_detail'),
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
 ]
