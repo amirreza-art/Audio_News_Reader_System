192d7185c8dfbaaddaa6543991e456da7a2e8a13
amirreza <taghaddosiamirreza@gmail.com>
2023-10-25 14:59:39 +0330

    edit <Category List API>: fix filtering by giving last_update

--- backend/news/views.py
+++ backend/news/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from .models import News, Category
 from .serializers import NewsByCategorySerializer, NewsSerializer, CategorySerializer
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from django.http import Http404
 import django_filters
 
@@ -41,15 +40,26 @@ class NewsDetailView(generics.RetrieveAPIView):
 
 
 class CategoryFilter(django_filters.FilterSet):
-    last_update = django_filters.DateFilter(field_name='news_items__updated_at', lookup_expr="exact")
+    last_update = django_filters.DateTimeFilter(field_name='news_items__updated_at__date', lookup_expr="exact")
    
     class Meta:
         model = Category
         fields = ('title', 'last_update')
 
+    @property
+    def qs(self):
+        queryset = Category.objects.all()
+        title = self.request.query_params.get('title')
+        if title:
+            queryset = queryset.filter(title=title).distinct()
+
+        last_update = self.request.query_params.get('last_update')
+        if last_update:
+            queryset = queryset.filter(news_items__updated_at__date=last_update).distinct()
+
+        return queryset
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    # filterset_fields = ("title", 'last_update')
     filterset_class = CategoryFilter
